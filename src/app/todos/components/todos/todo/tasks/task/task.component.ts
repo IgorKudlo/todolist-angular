b026d424e3b6baefa09aec2d5d60dd9679aa0dc8
igor.kudlo <igoreklida@gmail.com>
2024-01-19 17:14:40 +0300

    change task title

--- src/app/todos/components/todos/todo/tasks/task/task.component.ts
+++ src/app/todos/components/todos/todo/tasks/task/task.component.ts
@@ -16,6 +16,8 @@ export class TaskComponent {
     model: UpdateTaskModel;
   }>();
 
+  isEditMode = false;
+  taskTitle = '';
   taskStatusEnum = TaskStatusEnum;
 
   removeTaskHandler() {
@@ -24,15 +26,29 @@ export class TaskComponent {
 
   changeTaskStatusHandler(event: MouseEvent) {
     const newStatus = (event.currentTarget as HTMLInputElement).checked;
+    this.changeTask({ status: newStatus ? TaskStatusEnum.completed : TaskStatusEnum.active });
+  }
 
+  showActiveMode() {
+    this.isEditMode = true;
+    this.taskTitle = this.task.title;
+  }
+
+  changeTaskTitleHandler() {
+    this.changeTask({ title: this.taskTitle });
+    this.isEditMode = false;
+  }
+
+  changeTask(patch: Partial<UpdateTaskModel>) {
     const model: UpdateTaskModel = {
-      status: newStatus ? TaskStatusEnum.completed : TaskStatusEnum.active,
+      status: this.task.status,
       title: this.task.title,
       completed: this.task.completed,
       startDate: this.task.startDate,
       priority: this.task.priority,
       description: this.task.description,
       deadline: this.task.deadline,
+      ...patch,
     };
     this.changeTaskEvent.emit({ todolistId: this.task.todoListId, taskId: this.task.id, model });
   }
