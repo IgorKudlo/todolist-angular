cd69c3768a54f11af74e07e178adb470a6b435fb
igor.kudlo <igoreklida@gmail.com>
2024-01-21 23:45:36 +0300

    login, logout

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'tl-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9.%-]+\\.[a-z]{2,5}$'),
+    ]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    rememberMe: new FormControl(false),
+  });
+
+  constructor(private authService: AuthService) {}
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onLoginSubmit() {
+    const value = this.loginForm.value;
+    console.log(JSON.stringify(value));
+    this.authService.login(value);
+  }
+}
--- src/app/todos/components/todos/todos.component.ts
+++ src/app/todos/components/todos/todos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TodosService } from '../../services/todos.service';
 import { Todo } from '../../models/todos.models';
 import { Observable } from 'rxjs';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'tl-todos',
@@ -12,7 +13,10 @@ export class TodosComponent implements OnInit {
   todos$?: Observable<Todo[]>;
   todoTitle = '';
 
-  constructor(private todosService: TodosService) {}
+  constructor(
+    private todosService: TodosService,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit() {
     this.todos$ = this.todosService.todos$;
@@ -30,5 +34,9 @@ export class TodosComponent implements OnInit {
 
   changeTodoTitle(data: { todoId: string; todoTitle: string }) {
     this.todosService.updateTodoTitle(data.todoId, data.todoTitle);
+  }
+
+  logoutHandler() {
+    this.authService.logout();
   }
 }
